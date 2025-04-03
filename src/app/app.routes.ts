64c2722f74539c74e660d8d1e6edca77e9672622
victor <gfeng005@uottawa.ca>
2025-04-03 18:52:41 -0400

    updated the routes for edit component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,21 @@
 import { Routes } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 export const routes: Routes = [
-  { path: 'contacts', component: ContactsComponent, title: 'Contacts' },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    title: 'Contacts',
+  },
+  {
+    path: 'contacts/edit/:id',
+    component: EditContactComponent,
+    title: 'Contacts - Edit',
+  },
+  {
+    path: 'contacts/edit',
+    component: EditContactComponent,
+    title: 'Contacts - Edit',
+  },
 ];
