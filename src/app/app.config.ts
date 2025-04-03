d7a8cb5f0d91242d8335856a0de17f3cfc035d24
victor <gfeng005@uottawa.ca>
2025-04-03 18:52:24 -0400

    updated configuration for In memory contacts api.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { InMemoryContactsApi } from './services/in-memory-contacts.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, { delay: 0 })
+    ),
+  ],
 };
