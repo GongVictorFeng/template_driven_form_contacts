8173cd7e52c3f2682182f295448dbcad01d5c5b4
victor <gfeng005@uottawa.ca>
2025-04-04 21:41:15 -0400

    added data binding to the form.

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Contact } from '../models/contact.model';
+import { ContactsService } from '../services/contacts.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-contact.component.html',
-  styleUrl: './edit-contact.component.scss'
+  styleUrl: './edit-contact.component.scss',
 })
-export class EditContactComponent {
+export class EditContactComponent implements OnInit {
+  contact: Contact = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: 0,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    const contactId = this.route.snapshot.params['id'];
+    this.contactsService
+      .getContact(contactId)
+      .subscribe((contact) => (this.contact = contact));
+  }
+
+  onSave(form: NgForm) {
+    console.log(form.value);
+    this.contactsService
+      .saveContact(form.value)
+      .subscribe(() => this.router.navigate(['/contacts']));
+  }
 }
