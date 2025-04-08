926141f5d786008851c1997eb79a4ef81a3118b5
victor <gfeng005@uottawa.ca>
2025-04-08 16:37:30 -0400

    added a date value constrol accessor.

---
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -0,0 +1,45 @@
+import {
+  Directive,
+  ElementRef,
+  forwardRef,
+  HostListener,
+  Provider,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => DateValueAccessorDirective),
+  multi: true,
+};
+
+@Directive({
+  selector:
+    'input([type=date])[ngModel], input([type=date])[formControl], input([type=date])[formControlName]',
+  standalone: true,
+  providers: [DATE_VALUE_PROVIDER],
+})
+export class DateValueAccessorDirective implements ControlValueAccessor {
+  constructor(private elem: ElementRef) {}
+
+  @HostListener('input', ['$event.target.valueAsDate'])
+  private onChange!: Function;
+
+  @HostListener('blur')
+  private onTouched!: Function;
+
+  writeValue(date: Date | null): void {
+    if (date instanceof Date) {
+      this.elem.nativeElement.value = date.toISOString().split('T')[0];
+    }
+  }
+
+  registerOnChange(fn: Function): void {
+    this.onChange = (date: Date) => {
+      fn(date);
+    };
+  }
+  registerOnTouched(fn: Function): void {
+    this.onTouched = fn;
+  }
+}
